5cc35f43d0284323bc997700fbd6e27a93a1ee5b
Chanon <CH.Chanonlienwork@gmail.com>
2025-09-05 15:30:31 +0700

    Clear code console.log [client]#1

--- client/src/pages/layout/Dashboard.tsx
+++ client/src/pages/layout/Dashboard.tsx
@@ -1,10 +1,6 @@
 import { Outlet } from "react-router-dom"
 
-import { useSelector } from "react-redux"
-import type { RootState } from "@/store/store"
-
 const DashBoard = () => {
-  const userId = useSelector((state: RootState) => state.user.role)
   
   return (
     <section>
