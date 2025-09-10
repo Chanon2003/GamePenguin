db5858da753eccce0f02fd7968fa7b7e8d2f477f
Chanon <CH.Chanonlienwork@gmail.com>
2025-09-10 18:09:03 +0700

    Change : export defult in auth to normal export bc add new authAdmin

--- server/src/routes/user.routes.ts
+++ server/src/routes/user.routes.ts
@@ -1,12 +1,12 @@
 import { Router } from "express";
 import { getAllUser, refreshToken, signinEmail, signout, signupEmail } from "../controllers/user.controller";
-import auth from "../middleware/auth";
+import {auth, authAdmin} from "../middleware/auth";
 
 const userRouter = Router();
 
 userRouter.post("/signup-email",signupEmail);
 userRouter.post("/signin-email",signinEmail);
-userRouter.get("/getUsers",auth,getAllUser);
+userRouter.get("/getUsers",auth,authAdmin,getAllUser);
 userRouter.get("/logout",auth,signout);
 userRouter.post("/refresh-token",auth,refreshToken);
 
